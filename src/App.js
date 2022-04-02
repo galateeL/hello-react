6653c134378649bf0846459cc4f18fdcecbb12d6
Galatée Lardière <galatee.lardiere@gmail.com>
2022-04-02 11:50:00 +0200

    Hello react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WelcomeMessage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WelcomeMessage() {
+  return (
+    <div>
+      <h1>Hello Person!</h1>
+    </div>
+  );
+}
+
+export default WelcomeMessage;
